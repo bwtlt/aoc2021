f68c2259162645414a2bb333197b83e639ddf708
bwtlt <bertrandwatelet@gmail.com>
2021-12-08 18:32:24 +0100

    day5 parts 1 & 2

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -12,6 +12,20 @@ struct Point {
 #[derive(Debug)]
 struct Vent {
     points: Vec<Point>,
+    direction: Direction,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum Direction {
+    Left,
+    Right,
+    Up,
+    Down,
+    UpLeft,
+    UpRight,
+    DownLeft,
+    DownRight,
+    Other,
 }
 
 impl Vent {
@@ -40,49 +54,100 @@ impl Vent {
             x: end[0],
             y: end[1],
         };
-        let horizontal = start.x == end.x;
-        let vertical = start.y == end.y;
-        let positive = (horizontal && start.y < end.y) || (vertical && start.x < end.x);
-        if !horizontal && !vertical {
+        let horizontal = start.y == end.y;
+        let vertical = start.x == end.x;
+        let diagonal =
+            (start.x as i32 - end.x as i32).abs() == (start.y as i32 - end.y as i32).abs();
+        if !horizontal && !vertical && !diagonal {
             return None;
         }
-        let mut vent = Vent { points: Vec::new() };
+        let direction = if horizontal {
+            if start.x < end.x {
+                Direction::Right
+            } else {
+                Direction::Left
+            }
+        } else if vertical {
+            if start.y < end.y {
+                Direction::Down
+            } else {
+                Direction::Up
+            }
+        } else if diagonal {
+            if start.x < end.x {
+                if start.y < end.y {
+                    Direction::DownRight
+                } else {
+                    Direction::UpRight
+                }
+            } else if start.y < end.y {
+                Direction::DownLeft
+            } else {
+                Direction::UpLeft
+            }
+        } else {
+            Direction::Other
+        };
+        let mut vent = Vent {
+            points: Vec::new(),
+            direction,
+        };
         vent.points.push(start);
         let mut point = start;
-        let step: i32 = if positive { 1 } else { -1 };
+        let x_step = match direction {
+            Direction::Left | Direction::UpLeft | Direction::DownLeft => -1,
+            Direction::Right | Direction::UpRight | Direction::DownRight => 1,
+            _ => 0,
+        };
+        let y_step = match direction {
+            Direction::Up | Direction::UpLeft | Direction::UpRight => -1,
+            Direction::Down | Direction::DownLeft | Direction::DownRight => 1,
+            _ => 0,
+        };
         while point != end {
-            if horizontal {
-                point.y = (point.y as i32 + step) as u32;
-            } else if vertical {
-                point.x = (point.x as i32 + step) as u32;
-            }
+            point.x = (point.x as i32 + x_step) as u32;
+            point.y = (point.y as i32 + y_step) as u32;
             vent.points.push(point);
         }
         Some(vent)
+    }
+
+    fn is_diagonal(&self) -> bool {
+        self.direction == Direction::UpLeft
+            || self.direction == Direction::UpRight
+            || self.direction == Direction::DownLeft
+            || self.direction == Direction::DownRight
     }
 }
 
 const ARRAY_SIZE: usize = 1000;
 
+fn compute_answer(vents: &[Vent], include_diagonals: bool) -> u32 {
+    let mut grid: Vec<Vec<Option<u32>>> = vec![vec![None; ARRAY_SIZE]; ARRAY_SIZE];
+    vents.iter().for_each(|vent| {
+        if vent.is_diagonal() && !include_diagonals {
+            return;
+        }
+        vent.points
+            .iter()
+            .for_each(|point| match grid[point.y as usize][point.x as usize] {
+                Some(v) => grid[point.y as usize][point.x as usize] = Some(v + 1),
+                None => grid[point.y as usize][point.x as usize] = Some(1),
+            })
+    });
+    grid.iter().flatten().fold(0, |acc, &x| match x {
+        Some(n) if n >= 2 => acc + 1,
+        _ => acc,
+    })
+}
+
 fn main() {
     if let Ok(lines) = read_lines("./input.txt") {
-        let vents = lines.filter_map(|line| Vent::parse(&line.unwrap()));
-        let mut grid: [[Option<u32>; ARRAY_SIZE]; ARRAY_SIZE] = [[None; ARRAY_SIZE]; ARRAY_SIZE];
-        vents.for_each(|vent| {
-            vent.points
-                .iter()
-                .for_each(|point| match grid[point.x as usize][point.y as usize] {
-                    Some(v) => grid[point.x as usize][point.y as usize] = Some(v + 1),
-                    None => grid[point.x as usize][point.y as usize] = Some(1),
-                })
-        });
-        let answer = grid.iter().flatten().fold(0, |acc, &x| {
-            match x {
-                Some(n) if n >=2 => acc+1,
-                _ => acc,
-            }
-        });
-        println!("Part1 answer {}", answer);
+        let vents = lines
+            .filter_map(|line| Vent::parse(&line.unwrap()))
+            .collect::<Vec<Vent>>();
+        println!("Part1 answer {}", compute_answer(&vents, false));
+        println!("Part2 answer {}", compute_answer(&vents, true));
     }
 }
 
